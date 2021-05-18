37ed466a8abbb35f6e762dee3c29d0c7995eba8a
eamarasco <eamarasc@ucalgary.ca>
2021-05-18 12:03:31 -0600

    Add Lab 4 exercises

---
+++ Lab4Demo.py
@@ -0,0 +1,76 @@
+# ENSF 592 Spring 2021
+# May 18 Lab 4
+# Documentation Demo Exercise
+
+
+# get_user_input prompts for any input entry and returns the input value and its type
+# Argument: Takes in a number to count which input prompt is being given
+# Returns the input value (str object) and its corresponding type (type object)
+def get_user_input(n):
+    """Prompts for any input entry and returns the input value and its type
+    
+        Parameters:
+            n (int): A positive integer to count which input prompt is being given
+
+        Returns:
+            entry (str): The string input received from the user 
+            type(entry): The type of the input
+    """
+    entry = input("Please type any entry #" + str(n + 1) + ": ")
+    print(type(entry))
+    print(type(type(entry)))
+    return entry, type(entry)
+
+
+
+
+# process_user_input prints a value that was previously input and its type
+# Argument: Takes in a number to count which input prompt is being printed, the original input value, and the type of the value
+# No return value
+def process_user_input(n, entry, type):
+    print("This is entry #" + str(n + 1) + ":", entry)
+    print("The type of entry #" + str(n + 1) + " is :", str(type))
+
+
+# Method 1:
+# Print a new line for spacing and then the method title
+print('\n')
+print("***METHOD 1***")
+# Prompt the user to enter their name, input function will return the value as a string
+input1 = input("Please enter your name: ") # Prompts for input
+print("This is the first input:", input1) # Prints the original input back to the terminal along with a statement
+
+
+# Method 2: continuous input via a while loop
+print('\n' * 2) # Print two lines for spacing
+print("***METHOD 2***")
+
+# Loop continuously until "x" is provided as the input
+while True:
+    input2 = input("I am looking for specific input. You must type x: ") # Prompt for user input
+    if input2 == "x":
+        break
+print("This is the second input: " + input2) # Print original value to user
+
+
+# Method 3: call functions
+print('\n' * 2) # Add header for spacing
+print("***METHOD 3***")
+num_of_repeats = 3  # set number of inputs to prompt for
+results = [] # create empty list to store inputs
+results_types = [] # create list for types
+
+# Call get_user_input three times, add values to the list for input and its corresponding type
+for i in range(num_of_repeats):
+    a, b = get_user_input(i)
+    results.append(a)
+    results_types.append(b)
+
+# Call process_user_input, pass input counter, the actual input, and the type of that input
+for i in range(num_of_repeats):
+    process_user_input(i,results[i], results_types[i])
+
+
+
+# Print out our documentation from above!
+print(get_user_input.__doc__)
---
+++ Lab4Exercise1.py
@@ -0,0 +1,31 @@
+# ENSF 592 Spring 2021
+# May 18 Lab 4
+# Exercise 1
+
+# Solve each question below using comprehensions or generator expressions.
+
+from itertools import count
+
+# 1. Construct a list consisting of the double of n for each n up to 50 that is divisible by 5. Print the list.
+
+
+
+# 2. Construct a generator expression for calculating all the powers of 8. Print the first 8 values of the expression.
+
+
+
+# 3. Given the list of complex numbers below, construct a dictionary that holds the real component as the key and imaginary component as the value.
+# Print the final dictionary.
+
+values = [2 + 4j, 6 + 3j, -5 + 1j, 3 + 5j, 4 + 2j]
+
+
+# 4. Construct a generator expression for calculating the first ten triangular numbers, starting with 0. Print the results.
+# Hint: 0, 0 + 1 = 1, 0 + 1 + 2 = 3, 0 + 1 + 2 + 3 = 6, etc.
+
+
+
+# 5. Given the list of coordinate tuples below, create a single set that lists each number that appears at least once.
+
+coord = [(0, 1), (1, 2), (3, 4), (4, 1), (5, 2), (3, 6), (-1, 9), (7, 5), (4, 8), (6, 1), (0, 2)]
+
