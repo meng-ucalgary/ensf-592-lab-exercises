4b0dccb71163c66a59b448c1e7463ada4b3803c9
eamarasco <eamarasc@ucalgary.ca>
2021-06-03 11:50:26 -0600

    Added Lab 9 Exercises and Data

---
+++ Lab9Exercises.py
@@ -0,0 +1,22 @@
+# ENSF 592 Spring 2021
+# June 3 Lab 9
+# Exercises
+ 
+import numpy as np
+import pandas as pd
+
+# Example continued from Lab 6/7/8
+# Refer to the weather data stored in Lab9Data.xlsx
+
+
+# Demo: creating/exporting a multi-dimensional DataFrame from Excel
+
+
+# Add a column to calculate the delta for day (difference between max and min)
+
+
+# Find the average measurements for each month (across both years)
+
+
+# Print the data in both locations on May 1, 2021
+
